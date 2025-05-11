913569a9dc867f29131a8195dc529ae018b1899c
Swapn Gupta <b22ch038@iitj.ac.in>
2025-05-11 18:41:23 +0530

    do it

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Welcome to our custom app")
 st.header("i am learning Streamlit")
 st.subheader("i am loving it!")
+st.write('Hello swapn this side')
+
+st.markdown("""
+### my favorite foods are:
+- pizza
+- burger
+- fries
+""")
+
+st.code("""
+def foo():
+    return "foo**2"
+    
+x = foo(2)
+""")
+
+st.latex('x^2+y^2 = z^2')
+
+df = pd.DataFrame({
+    'name':['swapn','swapn','swapn'],
+    'marks':[100,90,80],
+    'package':[22,41,34]
+})
+st.dataframe(df)
+st.metric('Revenue','Rs 20 lakhs','3%')
+
+st.json({'name':'swapn','age':22})
+
+st.image('stuff.png')
