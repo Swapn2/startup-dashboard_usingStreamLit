62752bea395138d22a20e87db701ff38c09c957e
Swapn Gupta <b22ch038@iitj.ac.in>
2025-05-11 21:57:26 +0530

    do it

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-file = st.file_uploader('upload file')
+df = pd.read_csv('startup_funding.csv')
+# st.dataframe(df)
+df['Investors Name'] = df['Investors Name'].fillna('undisclosed')
 
-if file is not None:
-    df = pd.read_csv(file)
-    st.dataframe(df.describe())
+st.sidebar.title('startup Funding Analysis')
 
+option = st.sidebar.selectbox('select one ',['overall analysis','startup','Investor'])
 
-
+if option == 'overall analysis':
+    st.title('overall analysis')
+elif option == 'startup':
+    st.sidebar.selectbox('select startup',sorted(df['Startup Name'].unique().tolist()))
+    btn1 = st.sidebar.button('find startup details')
+    st.title('startup analysis')
+else:
+    st.sidebar.selectbox('select investor',sorted(df['Investors Name'].unique().tolist()))
+    btn2 = st.sidebar.button('investor analysis')
+    st.title('investor analysis')
--- streamlit_doc.py
+++ streamlit_doc.py
@@ -77,3 +77,14 @@ if btn:
     else:
         st.error('invalid credentials')
 
+import streamlit as st
+import pandas as pd
+
+file = st.file_uploader('upload file')
+
+if file is not None:
+    df = pd.read_csv(file)
+    st.dataframe(df.describe())
+
+
+
